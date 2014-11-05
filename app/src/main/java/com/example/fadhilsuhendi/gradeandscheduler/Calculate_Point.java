b4460b4bee2af1fce39ec33478d8cdef508060bf
Fadhil Suhendi <fadhilfathurrahman@rocketmail.com>
2014-11-05 15:43:15 -0700

    I just add all the file

--- app/src/main/java/com/example/fadhilsuhendi/gradeandscheduler/Calculate_Point.java
+++ app/src/main/java/com/example/fadhilsuhendi/gradeandscheduler/Calculate_Point.java
@@ -3,12 +3,24 @@ package com.example.fadhilsuhendi.gradeandscheduler;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
  * Created by fadhilsuhendi on 10/19/14.
+ * this is where all of the calculation for grade begins
  */
 public class Calculate_Point extends ActionBarActivity {
+
+    private Button calculateButton = null;
+    private EditText pointEarn = null;
+    private EditText classPerc = null;
+    private EditText totalPoint = null;
+    private TextView result = null;
+
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
@@ -16,5 +28,45 @@ public class Calculate_Point extends ActionBarActivity {
         setContentView(R.layout.calculate_grade_layout);
 
 
+        result = (TextView) findViewById(R.id.result_text);
+        pointEarn = (EditText) findViewById(R.id.point_earned);
+        classPerc = (EditText) findViewById(R.id.class_perc);
+        totalPoint = (EditText) findViewById(R.id.total_points);
+
+        calculateButton = (Button) findViewById(R.id.calculateButton);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                double resultVal,pt_earned,cl_perc,tot_point;
+
+
+                pt_earned = Double.parseDouble(pointEarn.getText().toString());
+                cl_perc = Double.parseDouble(classPerc.getText().toString());
+                tot_point = Double.parseDouble(totalPoint.getText().toString());
+
+
+
+                resultVal = pt_earned + cl_perc + tot_point;
+
+                result.setText(resultVal + ""); // easy way to convert string
+
+
+
+
+
+
+            }
+        });
+
+
     }
+
+    /*public void calculation() {
+
+
+
+
+    }*/
 }
