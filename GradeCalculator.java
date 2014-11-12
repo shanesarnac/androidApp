202a7f2cbac95cfb0d3771ac8ad92d1a6ced82ef
Shane Sarnac <shane.sarnac@colorado.edu>
2014-11-11 18:26:03 -0700

    Fixed the loop so that it will actually ask if the input is correct

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -25,14 +25,14 @@ public class GradeCalculator
 		
 		choice = "";
 		p.set_Class_Name(className);
-		/*while(p.is_complete() == false)
+		while(p.is_complete() == false)
 		{
 			while(choice != "q")
 			{
 				System.out.println("Enter the Name of the Category to be added: ");
 				categoryName = in.nextLine();
 				System.out.println("Enter the weight of the Category to be added: ");
-				Weight = in.nextDouble();
+				Weight = Double.parseDouble(in.nextLine());
 				System.out.println("You entered: " + categoryName + " for the category name and " + Weight + " for the weight.");
 				System.out.println("Is this correct (y/n)");
 				choice = in.nextLine();
@@ -41,16 +41,10 @@ public class GradeCalculator
 					choice = "q";
 				}
 			}
+			choice = "";
 			p.add_Category(categoryName, Weight);
-		}*/
-		p.add_Category("Quiz", 20.0);
-		p.add_Category("Midterms", 15.0);
-		p.add_Category("Written Homework", 30.0);
-		p.add_Category("Final", 35.0);
-		for(int i = 0; i < 4; i++)
-		{
-			p.get_Category(i).add_Assignment("Bullshit", 90.0, 100.0);
 		}
+		/* To Do: Add loops to allow for assignments to be added */
 		p.Save_Subject();
 		Calculate(p);
 	}
